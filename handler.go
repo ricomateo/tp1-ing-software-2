c1b0dfe8bc908fbbc5dea03516c80dd2dba636fa
ricomateo <mrico@fi.uba.ar>
2025-03-15 15:28:07 -0300

    move handlers to handler.go and add state

---
+++ handler.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateCourseHandler(c *gin.Context, state *State) {
+	course := Course{}
+	if err := c.ShouldBind(&course); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"title":    "Bad request",
+			"type":     "about:blank",
+			"status":   http.StatusBadRequest,
+			"detail":   "Could not create the course",
+			"instance": "/courses",
+		})
+		return
+	}
+	course.Id = state.IdCounter
+
+	// Store the course
+	state.Courses = append(state.Courses, course)
+
+	c.JSON(http.StatusCreated, gin.H{
+		"data": course,
+	})
+
+	// Increment the counter
+	state.IdCounter += 1
+}
+
+func GetCoursesHandler(c *gin.Context, state *State) {
+	c.JSON(http.StatusCreated, gin.H{
+		"data": state.Courses,
+	})
+}
--- main.go
+++ main.go
@@ -1,50 +1,27 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	state := State{
+		Courses:   make([]Course, 0),
+		IdCounter: uint64(0),
+	}
 
-	idCounter := uint64(0)
-	courses := make([]Course, 0)
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{"https://editor.swagger.io"}
 
 	r.Use(cors.New(config))
+
 	r.POST("/courses", func(c *gin.Context) {
-		course := Course{}
-		if err := c.ShouldBind(&course); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"title":    "Bad request",
-				"type":     "about:blank",
-				"status":   http.StatusBadRequest,
-				"detail":   "Could not create the course",
-				"instance": "/courses",
-			})
-			return
-		}
-		course.Id = idCounter
-
-		// Store the course
-		courses = append(courses, course)
-
-		c.JSON(http.StatusCreated, gin.H{
-			"data": course,
-		})
-
-		// Increment the counter
-		idCounter += 1
+		CreateCourseHandler(c, &state)
 	})
-
 	r.GET("/courses", func(c *gin.Context) {
-		c.JSON(http.StatusCreated, gin.H{
-			"data": courses,
-		})
+		GetCoursesHandler(c, &state)
 	})
 
 	r.Run()
