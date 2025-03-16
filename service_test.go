1a2b7f2cc4ea06c95edaac59abbf1664b33d0d23
ricomateo <mrico@fi.uba.ar>
2025-03-16 18:23:43 -0300

    add test for GET /courses/{id} endpoint

--- service_test.go
+++ service_test.go
@@ -43,7 +43,31 @@ func TestCreateCourse(t *testing.T) {
 		resp.Body.Close()
 	})
 
-	t.Run("Check that different courses have different ids", func(t *testing.T) {
+	t.Run("Get course with id 0", func(t *testing.T) {
+		id := "0"
+		resp, err := http.Get(url + "/" + id)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusCreated, resp.StatusCode)
+
+		expectedResponse := `{"data":{"title":"test title","description":"test description","id":0}}`
+		body, _ := io.ReadAll(resp.Body)
+		assert.Equal(t, expectedResponse, string(body))
+		resp.Body.Close()
+	})
+
+	t.Run("Check that an error is returned when requesting a non existing course", func(t *testing.T) {
+		id := "1"
+		resp, err := http.Get(url + "/" + id)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+
+		expectedResponse := `{"detail":"The course with ID 1 was not found","instance":"/courses/1","status":404,"title":"Course not found","type":"about:blank"}`
+		body, _ := io.ReadAll(resp.Body)
+		assert.Equal(t, expectedResponse, string(body))
+		resp.Body.Close()
+	})
+
+	t.Run("Check that courses are created with incremental ids", func(t *testing.T) {
 		course := `{"title":"test title 2","description":"test description 2"}`
 		resp, err := http.Post(url, "application/json", strings.NewReader(course))
 		assert.NoError(t, err)
