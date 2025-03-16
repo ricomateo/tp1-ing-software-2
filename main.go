f6f24bfe70a08549eb00810bf4d2bba5ba468432
ricomateo <mrico@fi.uba.ar>
2025-03-16 15:53:46 -0300

    add function to start the service

--- main.go
+++ main.go
@@ -3,12 +3,7 @@ package main
 import (
 	"log"
 	"os"
-
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
 )
-
-const SWAGGER_URL string = "https://editor.swagger.io"
 
 func main() {
 	host := os.Getenv("HOST")
@@ -19,23 +14,6 @@ func main() {
 	if port == "" {
 		log.Fatal("Missing required environment variable PORT")
 	}
-	r := gin.Default()
-	state := State{
-		Courses:   make([]Course, 0),
-		IdCounter: uint64(0),
-	}
 
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{SWAGGER_URL}
-
-	r.Use(cors.New(config))
-
-	r.POST("/courses", func(c *gin.Context) {
-		CreateCourseHandler(c, &state)
-	})
-	r.GET("/courses", func(c *gin.Context) {
-		GetCoursesHandler(c, &state)
-	})
-	address := host + ":" + port
-	r.Run(address)
+	StartService(host, port)
 }
