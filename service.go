26e20ab740df3efaf6e805a83773ec5974bd976a
ricomateo <mrico@fi.uba.ar>
2025-03-17 19:34:22 -0300

    disable cors

--- service.go
+++ service.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
-
-const SWAGGER_URL string = "https://editor.swagger.io"
 
 func StartService(host, port string) {
 	r := gin.Default()
@@ -13,11 +10,6 @@ func StartService(host, port string) {
 		Courses:   make([]Course, 0),
 		IdCounter: uint64(0),
 	}
-
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{SWAGGER_URL}
-
-	r.Use(cors.New(config))
 
 	r.POST("/courses", func(c *gin.Context) {
 		CreateCourseHandler(c, &state)
