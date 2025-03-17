8d9866d5a83d79ea4801fc35a7df87086d6335ca
ricomateo <mrico@fi.uba.ar>
2025-03-17 20:13:18 -0300

    move utils functions to utils.go

--- course.go
+++ course.go
@@ -5,18 +5,3 @@ type Course struct {
 	Description string `json:"description" binding:"required"`
 	Id          uint64 `json:"id"`
 }
-
-// Receives an array of courses and returns an array of courses in the inverse order.
-func ReverseCourses(courses []Course) []Course {
-	lenCourses := len(courses)
-	reversedCourses := make([]Course, lenCourses)
-	for i := 0; i < len(courses); i++ {
-		reversedCourses[i] = courses[lenCourses-1-i]
-	}
-	return reversedCourses
-}
-
-// Receives an array of courses and removes the course at the given index.
-func RemoveCourseWithIndex(courses []Course, index int) []Course {
-	return append(courses[:index], courses[index+1:]...)
-}
--- utils.go
+++ utils.go
@@ -14,3 +14,18 @@ func GetEnvVar(variable string) (string, error) {
 	}
 	return value, nil
 }
+
+// Receives an array of courses and returns an array of courses in the inverse order.
+func ReverseCourses(courses []Course) []Course {
+	lenCourses := len(courses)
+	reversedCourses := make([]Course, lenCourses)
+	for i := 0; i < len(courses); i++ {
+		reversedCourses[i] = courses[lenCourses-1-i]
+	}
+	return reversedCourses
+}
+
+// Receives an array of courses and removes the course at the given index.
+func RemoveCourseWithIndex(courses []Course, index int) []Course {
+	return append(courses[:index], courses[index+1:]...)
+}
